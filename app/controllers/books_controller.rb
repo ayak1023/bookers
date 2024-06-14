1e029121c553c1ac51206f4ea90e75827a74d7c2
EC2 Default User <ec2-user@ip-172-31-25-119.ap-northeast-1.compute.internal>
2024-06-14 03:03:09 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  #get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
   get '/' => 'homes#top'
-  get '/books' => 'books#index'
+  get 'books' => 'books#index'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
