da5bb8e517df31069d8a6692268d0dfd01c62d98
EC2 Default User <ec2-user@ip-172-31-25-119.ap-northeast-1.compute.internal>
2024-06-16 00:33:49 +0000

    [Update]root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   get '/' => 'homes#top'
+  root 'homes#top'
   get 'books' => 'books#index'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  #get 'books/:id/edit' => 'books#edit', as: 'book_list'
+
   resources :books
+  #index, new, create, edit, update, show, delete
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
